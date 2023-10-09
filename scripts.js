a741ef1a4ebd38ad1fca444c8d827eda87587b14
ivg1 <132875824+ivg1@users.noreply.github.com>
2023-10-09 11:55:32 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 setTimeout(showPage, 4000);
 function showPage(){
-  window.location.replace("http://www.ivg1.github.io/Local/");
+  window.location.replace("http://www.ivg1.github.io/Local/index.html");
 }
